bbe63046ad4ea028158905e3a1df9f808694aed9
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-10 01:54:52 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+if __name__ == '__main__':
+    from sys import argv
+    n = len(argv)
+    if n == 2:
+        print('{} argument:'.format(1))
+    elif n > 2:
+        print('{} arguments:'.format(n - 1))
+    else:
+        print('{} arguments.'.format(0))
+    for i in range(1, n):
+        print('{}: {}'.format(i, argv[i]))
