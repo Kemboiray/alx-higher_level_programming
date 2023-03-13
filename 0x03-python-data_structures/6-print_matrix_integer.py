b458c822a7d28d72345df24c7ef6d58e9e70793c
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-13 21:25:53 +0300

    Use sep

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,4 @@
 # 6-print_matrix_integer.py
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            if j == i[-1]:
-                print('{:d}'.format(j), end='')
-            else:
-                print('{:d}'.format(j), end=', ')
-        print()
+        print(*i, sep=', ')
