09072ec55511af19455b61545f214c42f766d77a
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-13 22:44:57 +0300

    UUpdate 7-add*.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+# 7-add_tuple.py
+def add_tuple(tuple_a=(), tuple_b=()):
+    '''
+    Adds corresponding elements of two tuples, `tuple_a` and `tuple_b`
+
+    Return: a tuple of the sums
+    '''
+    a = (0, 0)
+    tuple_a += a
+    tuple_b += a
+    x = tuple_a[0] + tuple_b[0]
+    y = tuple_a[1] + tuple_b[1]
+    return (x, y)
