d2716834d3eba072e2ea8ea3055f6e1db283964b
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-13 23:07:02 +0300

    Update 9-max*.c

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+# 9-max_integer.py
+def max_integer(my_list=[]):
+    '''
+    Returns the maximum value in a list of integers,
+    or `None` if the list is empty
+    '''
+    if len(my_list) == 0:
+        return None
+    else:
+        return (sorted(my_list))[-1]
