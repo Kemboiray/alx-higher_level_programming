b876d6333e459a4481b24a7a81e7477c8ec12867
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-13 23:27:07 +0300

    Update 10-divisible*.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+# 10-divisible_by_2.py
+def divisible_by_2(my_list=[]):
+    '''
+    Finds all multiples of 2 in a list
+    Args:
+        my_list: list
+    Return:
+        Truth list with corresponding elements `True` or `False`
+    '''
+    new_list = my_list.copy()
+    for i in range(0, len(my_list)):
+        new_list[i] = (my_list[i] % 2 == 0)
+    return new_list
