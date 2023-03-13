50dc07a65d84c3204e4c7304f82d3987c8d06e72
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-13 22:57:15 +0300

    Update 8-miltiple*.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+# 8-multiple_returns.py
+def multiple_returns(sentence):
+    '''
+    Returns a tuple consisting of the length of a string
+    and its first character (which is set to `None` in the
+    case of an empty string)
+
+    Args:
+        `sentence`: string
+    '''
+    str_len = len(sentence)
+    if str_len == 0:
+        return (str_len, None)
+    else:
+        return (str_len, sentence[0])
