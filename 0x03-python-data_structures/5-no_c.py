8fe131fa0072708c213dbe3945652da63ddab56e
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-13 16:43:21 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+# 5-no_c.py
+def no_c(my_string):
+    '''
+    removes all characters c and C from a string
+    '''
+    my_list = []
+    for i in my_string:
+        if i.lower() != 'c':
+            my_list.append(i)
+    new_string = my_list[0]
+    for i in range(1, len(my_list)):
+        new_string += my_list[i]
+    return new_string
