3a57b412a748b50031959d66f1692a88e082307a
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-13 12:57:47 +0300

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+# 1-element_at.py
+def element_at(my_list, idx):
+    '''
+    Returns element at a given index, `idx` in a list, `my_list`
+    If `idx` is negative or out of bounds, returns `None`
+    '''
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
