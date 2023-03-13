ae3554f5fe603b900642a5e71c64c4ea0f567d66
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-13 21:57:02 +0300

    Separate using spaces, not comma

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,9 @@
 # 6-print_matrix_integer.py
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        print(*i, sep=', ')
+        for j in i:
+            if j == i[-1]:
+                print('{:d}'.format(j), end='')
+            else:
+                print('{:d}'.format(j), end=' ')
+        print()
