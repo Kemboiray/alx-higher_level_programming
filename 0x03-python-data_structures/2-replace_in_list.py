33cff7533fda845428f1a79ab0bfb8a468f40547
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-13 13:29:16 +0300

    Update 2-replace*.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+# 2-replace_in_list.py
+def replace_in_list(my_list, idx, element):
+    '''
+    replaces an element, `element` of a list, `my_list`
+    at a specific position, `idx`
+    '''
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return my_list
