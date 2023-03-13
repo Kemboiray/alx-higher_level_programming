2b94c6e7bcdee75db58806202e79ae2c2e6f747b
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-13 23:54:59 +0300

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+# 11-delete_at.py
+def delete_at(my_list=[], idx=0):
+    '''
+    deletes the item at a specific position in a list
+    Args:
+        `my_list`: list
+        `idx`: position
+    Return:
+        modified list if valid index was given, original list otherwise
+    '''
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx:idx+1] = []
+    return my_list
