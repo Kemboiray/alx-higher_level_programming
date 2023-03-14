5a67a01f60a41a3961986ef9e3f6134c4c6eeffe
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-14 15:49:56 +0300

    Skip execution if NoneType is passed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,6 +5,8 @@ def print_reversed_list_integer(my_list=[]):
     '''
     prints all integers of a list, in reverse order
     '''
+    if not (isinstance(my_list, list)):
+        return
     my_list.reverse()
     for i in my_list:
         print('{:d}'.format(i))
