f335db9da528ee3dc8923df863920c65af3289f2
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-15 19:51:16 +0300

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if isinstance(my_list, list):
+        new = []
+        for i in my_list:
+            if i == search:
+                new.append(replace)
+            else:
+                new.append(i)
+        return new
