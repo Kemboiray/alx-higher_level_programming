ff01835206e32c8e8520e4aae68e72895419e6fb
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-15 20:15:20 +0300

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    if isinstance(set_1, set) and isinstance(set_2, set):
+        return set(
+                list(set_1.difference(set_2)) + list(set_2.difference(set_1)))
