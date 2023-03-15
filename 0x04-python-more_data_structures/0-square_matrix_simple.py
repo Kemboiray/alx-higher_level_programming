b799d287bf7cbca9b384ea71adaeb0bd3dffc008
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-15 19:37:00 +0300

    pycodestyle correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    if isinstance(matrix, list) and all(isinstance(row, list) for \
-            row in matrix):
+    if isinstance(
+            matrix, list) and all(isinstance(row, list) for row in matrix):
         new = matrix.copy()
         for i in range(len(matrix)):
             new[i] = list(map(lambda x: x**2, matrix[i]))
