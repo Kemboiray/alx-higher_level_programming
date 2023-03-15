a3a6edb9eef06d3cf5ed5de8b365c76070ece0de
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-15 19:59:50 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    if isinstance(my_list, list):
+        my_set = set(my_list)
+        summa = 0
+        for i in my_set:
+            summa += i
+        return summa
