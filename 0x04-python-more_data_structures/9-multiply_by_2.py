6d221146df2301d4dfe1c018e12db72b826cce39
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-15 22:03:29 +0300

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    if isinstance(a_dictionary, dict):
+        new = a_dictionary.copy()
+        for key in new:
+            new.update({key: new[key] * 2})
+        return new
