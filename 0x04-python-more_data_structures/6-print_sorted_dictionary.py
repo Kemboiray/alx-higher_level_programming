29bdfa45e66c4a5186a4a8e93c10426f97727f18
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-15 20:56:19 +0300

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    if isinstance(a_dictionary, dict):
+        to_print = sorted(a_dictionary)
+        for i in to_print:
+            print(f'{i}: {a_dictionary[i]}')
