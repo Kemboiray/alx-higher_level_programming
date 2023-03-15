502d1568f690ccdcf3ee2ff931da94a00db31487
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-15 22:20:44 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if isinstance(a_dictionary, dict):
+        for key in a_dictionary:
+            best = sorted(a_dictionary.values(), reverse=True)[0]
+            if a_dictionary[key] == best:
+                return key
+    else:
+        return None
