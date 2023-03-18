3be63f21d94887556d4c7a213641daddbf382548
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-18 16:58:39 +0300

    Remove dynamic memory allocation

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,12 +4,11 @@
  * is_palindrome - checks whether a linked list is a palindrome
  * @head: Address of pointer to the head of the linked list
  *
- * Return: 1 (true), 0 (false) or -1 (memory allocation failure).
+ * Return: 1 (true) or 0 (false).
  */
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp = *head;
-	int *arr = NULL;
 	unsigned int len, i;
 
 	if (temp == NULL)
@@ -18,9 +17,7 @@ int is_palindrome(listint_t **head)
 	for (len = 0; temp; len++, temp = temp->next)
 		;
 
-	arr = malloc(len * sizeof(int));
-	if (arr == NULL)
-		return (-1);
+	int arr[len];
 
 	for (i = 0, temp = *head; temp; i++, temp = temp->next)
 		arr[i] = temp->n;
@@ -28,12 +25,8 @@ int is_palindrome(listint_t **head)
 	for (i = 0; i < len / 2; i++)
 	{
 		if (arr[i] != arr[len - 1 - i])
-		{
-			free(arr);
 			return (0);
-		}
 	}
 
-	free(arr);
 	return (1);
 }
