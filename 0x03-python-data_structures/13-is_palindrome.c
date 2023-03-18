84d8f906339165d3346b5d8a381b8b2f377d42b1
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-03-18 17:05:56 +0300

    Checkout 65bfde2

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -4,11 +4,12 @@
  * is_palindrome - checks whether a linked list is a palindrome
  * @head: Address of pointer to the head of the linked list
  *
- * Return: 1 (true) or 0 (false).
+ * Return: 1 (true), 0 (false) or -1 (memory allocation failure).
  */
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp = *head;
+	int *arr = NULL;
 	unsigned int len, i;
 
 	if (temp == NULL)
@@ -17,7 +18,9 @@ int is_palindrome(listint_t **head)
 	for (len = 0; temp; len++, temp = temp->next)
 		;
 
-	int arr[len];
+	arr = malloc(len * sizeof(int));
+	if (arr == NULL)
+		return (-1);
 
 	for (i = 0, temp = *head; temp; i++, temp = temp->next)
 		arr[i] = temp->n;
@@ -25,8 +28,12 @@ int is_palindrome(listint_t **head)
 	for (i = 0; i < len / 2; i++)
 	{
 		if (arr[i] != arr[len - 1 - i])
+		{
+			free(arr);
 			return (0);
+		}
 	}
 
+	free(arr);
 	return (1);
 }
