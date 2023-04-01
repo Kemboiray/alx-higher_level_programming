fc917c1b8be5caa096845c7300456e38ac94da3c
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-01 14:34:54 +0300

    Rewrite code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,20 +2,24 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     """
-    prints the first x elements of a list and only integers
+    Prints the first x elements of a list skipping non-integers
 
-    Arg my_list: list of elements
-    Arg x: number of elements
-    Return: Real number of integers printed.
+    Arg my_list: given list
+    Arg x: number of elements to print
+
+    Return value: Number of elements printed.
     """
     count = 0
     for i in range(x):
+        elem = my_list[i]
         try:
-            print("{:d}".format(my_list[i]), end="")
-        except (ValueError, TypeError):
-            pass
-        else:
-            count += 1
-    if count:
-        print()
+            if not isinstance(elem, int):
+                pass
+            else:
+                print("{:d}".format(elem), end="")
+                count += 1
+        except ValueError:
+            print()
+            return count
+    print()
     return count
