2347ce375bbd9c3aeaf1373e3163b782336d27e4
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-01 13:10:12 +0300

    Print newline even if no element is printed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,8 +14,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
             count += 1
-    except Exception:
-        pass
-    if count:
+        print()
+    except (IndexError, ValueError):
         print()
     return count
