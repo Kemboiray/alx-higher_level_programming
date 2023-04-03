787c973d7672a0352345d82c6fa36d4713fc4720
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-03 16:33:20 +0300

    Do task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Definition of a class object Square"""
+
+
+class Square:
+    """Defines a square with a private instance attribute"""
+    def __init__(self, size=0):
+        """Initialises instance attribute
+
+        Args:
+            size (int): value with which to initialise attribute
+
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
