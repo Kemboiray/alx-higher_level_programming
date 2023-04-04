06f7e10efb4949c73f0f27bd4872e41dd75d8e2a
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-04 03:53:04 +0300

    Handle error raises in __init__ method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -13,8 +13,18 @@ class Rectangle:
             width (int): the width of the rectangle. Defaults to 0.
             height (int): the height of the rectangle. Defaults to 0.
         """
-        self.__width = width
-        self.__height = height
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = width
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = height
 
     @property
     def width(self):
