aaa48f870041a35f1496859a07cbf5cfc8179b30
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-11 06:20:18 +0300

    Attempt task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines ``add_integer()``"""
 
+
 def add_integer(a, b=98):
     """Adds two real numbers
     Args:
@@ -10,4 +11,8 @@ def add_integer(a, b=98):
         Integer result of the addition
     """
     if not isinstance(a, int) and not isinstance(a, float):
-
+        raise TypeError('a must be an integer')
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError('b must be an integer')
+    result = int(a) + int(b)
+    return result
