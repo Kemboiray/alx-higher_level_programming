e30c6039ccf1a0329ea9c17d28f91e081b98fe54
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-12 02:45:49 +0300

    Create 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines an empty class `BaseGeometry`"""
+
+
+class BaseGeometry:
+    """Class pending implementation of method, `area`"""
+    def area(self):
+        raise Exception('area() is not implemented')
