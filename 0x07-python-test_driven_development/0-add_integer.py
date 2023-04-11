9e6856072d3cd39c62daa5b1e6b1bdd929f6b4fe
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-11 09:45:04 +0300

    Checkout to state at commit id aaa48f8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,9 +10,9 @@ def add_integer(a, b=98):
     Returns:
         Integer result of the addition
     """
-    if a is None or (not isinstance(a, int) and not isinstance(a, float)):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
-    if b is None or (not isinstance(b, int) and not isinstance(b, float)):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError('b must be an integer')
     result = int(a) + int(b)
     return result
