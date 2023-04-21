241caeb665b8d51f14e6967d59a00bd96c66b38c
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-04-21 14:38:59 +0300

    Empty commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines the first class ``Base``"""
+import json
 
 
 class Base:
@@ -16,3 +17,19 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Return json string representation of a list of dictionaries
+        Arg:
+            list_dictionaries: a list of dictionaries
+        """
+        if not (isinstance(list_dictionaries, list)
+                or list_dictionaries is None):
+            raise TypeError(f'Expected a list, or None, \
+got {type(list_dictionaries)}')
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        for item in list_dictionaries:
+            if not isinstance(item, dict):
+                raise TypeError(f'Expected a dict, got {type(item)}')
+        return json.dumps(list_dictionaries)
