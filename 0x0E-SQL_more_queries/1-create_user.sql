6104fa7025b51a8b3b8000c8d1b8cba0a6dc0622
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-05-21 18:42:13 +0300

    Remove grant option to new user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Create a user and grant all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
