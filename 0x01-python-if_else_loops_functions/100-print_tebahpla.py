83387f3d9c35b9ed343fce7a9a8fba41ef6b2175
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-06-16 13:15:36 +0300

    Task 14

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+ascii_list = [122, 89, 120, 87, 118, 85, 116, 83, 114, 81, 112, 79, 110, 77,
+              108, 75, 106, 73, 104, 71, 102, 69, 100, 67, 98, 65]
+for num in ascii_list:
+    print('{}'.format(chr(num)), end='')
