300e10e90d05003c1303049e946d5aa0a775b036
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-06-17 22:58:29 +0300

    Use command-line arguments

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,8 +3,10 @@
 
 if __name__ == '__main__':
     import MySQLdb
+    from sys import argv
 
-    conn = MySQLdb.connect(host="localhost", port=3306, db="hbtn_0e_0_usa")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
