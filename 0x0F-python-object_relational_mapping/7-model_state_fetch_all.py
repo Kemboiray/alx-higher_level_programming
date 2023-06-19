75f8dc2356e02c84e64d15093d20b172ab857814
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-06-19 12:53:24 +0300

    Task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:'
+                           + f'{sys.argv[2]}@localhost/{sys.argv[3]}',
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
+        print(f'{state.id}: {state.name}')
+    session.close()
