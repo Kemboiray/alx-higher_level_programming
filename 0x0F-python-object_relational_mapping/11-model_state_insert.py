902579cd9f0465aaa639959e46f186b103ea0674
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-06-19 23:05:16 +0300

    Task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Lists all ``State`` objects from a database
+
+Usage:
+    $ ./11-model_state_insert.py <user_name> <password> <database_name>
+
+"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:'
+                           + f'{sys.argv[2]}@localhost/{sys.argv[3]}',
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+    session.close()
