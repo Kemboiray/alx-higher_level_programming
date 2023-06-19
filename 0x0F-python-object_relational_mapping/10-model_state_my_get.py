8cb488973f65a4973f7f788db03efd558e868650
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-06-19 22:43:38 +0300

    Task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lists the first ``State`` objects from a database
+
+Usage:
+    $ ./7-model_state_fetch_all.py <user_name> <password> <database_name>
+
+"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:'
+                           + f'{sys.argv[2]}@localhost/{sys.argv[3]}',
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter_by(name=sys.argv[4]).first()
+    if state:
+        print(f'{state.id}')
+    else:
+        print('Not found')
+    session.close()
