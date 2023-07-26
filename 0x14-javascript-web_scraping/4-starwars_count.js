739850abe842f064e73c6b496067b1ba8655f095
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-07-27 00:27:18 +0300

    Improve readability

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,21 +2,24 @@
 const request = require('request');
 const url = process.argv[2];
 
+// Make a request to the specified URL
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const data = JSON.parse(body);
-    console.log(filter(data));
+    const characterCount = countCharacterAppearances(data);
+    console.log(characterCount);
   }
 });
 
-function filter (obj) {
+// Count the number of times the specified character appears in the data
+function countCharacterAppearances (data) {
   let count = 0;
-  const key = 'https://swapi-api.alx-tools.com/api/people/18/';
-  const catalog = obj.results;
-  for (const film of catalog) {
-    if (film.characters.includes(key)) {
+  const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+  const films = data.results;
+  for (const film of films) {
+    if (film.characters.includes(characterUrl)) {
       count += 1;
     }
   }
