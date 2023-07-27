398b26a5e924a79ef23218f4068b89f0d7d57fef
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-07-27 13:55:01 +0300

    Swap files

--- 0x14-javascript-web_scraping/bing.js
+++ 0x14-javascript-web_scraping/bing.js
@@ -1,27 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
+const apiUrl = process.argv[2];
 
-request(url, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const users = JSON.parse(body);
-    const completions = {};
-    for (const user of users) {
-      const userId = JSON.stringify(user.userId);
-      if (userId in completions) {
-        if (user.completed === true) {
-          completions[userId] += 1;
-        }
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const todos = JSON.parse(body);
+  const completedTasks = {};
+  todos.forEach((todo) => {
+    if (todo.completed) {
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId] += 1;
       } else {
-        if (user.completed === true) {
-          completions[userId] = 1;
-        } else {
-          completions[userId] = 0;
-        }
+        completedTasks[todo.userId] = 1;
       }
     }
-    console.log(completions);
-  } else {
-    console.error(error);
-  }
+  });
+  console.log(completedTasks);
 });
