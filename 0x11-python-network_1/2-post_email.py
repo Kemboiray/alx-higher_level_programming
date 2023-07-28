ae61deed527cbe4353b7ed9d775fa82c3eb973b9
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-07-28 15:52:58 +0300

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Send a POST request and display the body of the response"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print(body)
