3b7a8ee4feba8d0cdcad3de1aad6df61ceaaa0c3
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-07-30 03:38:37 -0700

    see diff

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,8 +14,9 @@ if __name__ == "__main__":
     res = requests.post(url, data)
     try:
         d = res.json()
-    except Exception:
-        print("Not a valid JSON")
+    except Exception as e:
+        print e
+        # print("Not a valid JSON")
     else:
         if d:
             print(f"[{d['id']}] {d['name']}")
